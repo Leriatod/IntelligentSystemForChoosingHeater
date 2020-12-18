df96c0ef6102d68808c62811f9a2263fd1334572
Leriatod <artem.viznuk@gmail.com>
2020-12-18 17:49:16 +0200

    Displaying admin products.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[] = [];
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.products = products);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
