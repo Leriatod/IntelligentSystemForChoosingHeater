32257ee8e6f4987e837f98d08a042a71e00c62fc
Leriatod <artem.viznuk@gmail.com>
2021-01-08 20:03:07 +0200

    Added products page for user.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from '../models/product';
+import { Subscription } from 'rxjs';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  products: Product[];
+  subscription: Subscription;
+  isOrderedByPriceAsc: boolean = null;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.products = products);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  orderByPriceAsc() {
+    this.products = _.sortBy(this.products, p => p.price);
+    this.isOrderedByPriceAsc = true;
+  }
+
+  orderByPriceDesc() {
+    this.orderByPriceAsc();
+    this.products = this.products.reverse();
+    this.isOrderedByPriceAsc = false;
+  }
 }
