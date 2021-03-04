51784781e80c2430eca87fce3baf85b1a3857fd6
Leriatod <artem.viznuk@gmail.com>
2021-03-05 00:38:23 +0200

    Added filtering by category on home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
-import { ProductService } from './../product.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Product } from '../models/product';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import * as _ from 'underscore';
+
+import { CategoryService } from '../category.service';
+import { Product } from '../models/product';
+import { Category } from './../models/category';
+import { ProductService } from './../product.service';
 
 @Component({
   selector: 'home',
@@ -12,53 +15,73 @@ import * as _ from 'underscore';
 export class HomeComponent implements OnInit, OnDestroy {
   products: Product[];
   filteredProducts: Product[];
+  categories: Category[];
+  subscriptions: Subscription[] = [];
 
-  subscription: Subscription;
   filter: any = {
     isOrderedByPriceAsc: null,
-    query: ''
+    query: '',
+    category: ''
   }
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private categoryService: CategoryService) { }
 
   ngOnInit() {
-    this.subscription = this.productService.getAll()
-      .subscribe(products => this.filteredProducts = this.products = products);
+    this.loadProducts();
+    this.loadCategories();
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  private loadProducts() {
+    var subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+    this.subscriptions.push(subscription);
+  }
+
+  private loadCategories() {
+    var subscription = this.categoryService.getAll()
+      .subscribe(categories => this.categories = categories);
+    this.subscriptions.push(subscription);
   }
 
   resetFilters() {
     this.filter.isOrderedByPriceAsc = null;
     this.filter.query = '';
-    this.onQueryChange();
+    this.filter.category = '';
+    this.applyFiltering();
   }
 
-  onQueryChange() {
-    this.filteredProducts = this.products;
-    this.filteredProducts = _.filter(this.filteredProducts, p => { 
-      var title       = p.title.toLowerCase();
-      var template    = this.filter.query.toLowerCase();
-      var hasTemplate = title.includes(template);
-      return hasTemplate;
-    });
+  filterByCategory(category) { 
+    this.filter.category = category; 
+    this.applyFiltering();
   }
 
   orderByPriceAsc() {
     this.filter.isOrderedByPriceAsc = true;
-    this.applyOrderingByPrice();
+    this.applyFiltering();
   }
 
   orderByPriceDesc() {
     this.filter.isOrderedByPriceAsc = false;
-    this.applyOrderingByPrice();
+    this.applyFiltering();
   }
 
-  private applyOrderingByPrice() {
-    this.filteredProducts = _.sortBy(this.filteredProducts, p => p.price);
-    if (this.filter.isOrderedByPriceAsc) return;
-    this.filteredProducts = this.filteredProducts.reverse();
+  applyFiltering() {
+    this.filteredProducts = this.products;
+
+    this.filteredProducts = _.filter(this.filteredProducts, p => { 
+      var title       = p.title.toLowerCase();
+      var template    = this.filter.query.toLowerCase();
+      var hasTemplate = title.includes(template);
+      var hasCategory = p.category === this.filter.category || !this.filter.category;
+      return hasTemplate && hasCategory;
+    });
+    if (this.filter.isOrderedByPriceAsc === null) return;
+    var sign = 2 * this.filter.isOrderedByPriceAsc - 1;
+    this.filteredProducts = _.sortBy(this.filteredProducts, p => sign * p.price);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
