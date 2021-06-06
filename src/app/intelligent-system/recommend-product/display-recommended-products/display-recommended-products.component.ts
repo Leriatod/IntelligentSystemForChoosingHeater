034b87f895ac648005078a8b96874bfa80f99eb6
Leriatod <artem.viznuk@gmail.com>
2021-06-06 12:39:46 +0300

    Added tooltips for dialog answers.

--- src/app/intelligent-system/recommend-product/display-recommended-products/display-recommended-products.component.ts
+++ src/app/intelligent-system/recommend-product/display-recommended-products/display-recommended-products.component.ts
@@ -4,10 +4,10 @@ import { Product } from 'src/app/shared/models/product';
 @Component({
   selector: 'display-recommended-products',
   templateUrl: './display-recommended-products.component.html',
-  styleUrls: ['./display-recommended-products.component.scss']
+  styleUrls: ['./display-recommended-products.component.scss'],
 })
 export class DisplayRecommendedProductsComponent {
-  @Input() sortedProductsByMatchingFeaturesDesc: Product[] = [];
+  @Input() sortedProductsByTotalScoreDesc: Product[] = [];
   @Input() selectedFeatures = [];
   @Input() productsNumberToDisplay = 5;
 }
--- src/app/shared/models/feature-type.ts
+++ src/app/shared/models/feature-type.ts
@@ -1,16 +1,14 @@
-
 export interface FeatureType {
-    key: string, 
-    name: string,
-    question: string, // question to user if he needs to choose some feature from this category
-                      // used in dialog with user
-    canSelectManyFeatures: boolean, // true - rendering checkboxes, false - radio buttons
-    features: { 
-        [featureKey: string]: { 
-            answer: string, 
-            name: string 
-        } 
-    }
-
-
+  key: string;
+  name: string;
+  question: string; // question to user if he needs to choose some feature from this category
+  // used in dialog with user
+  canSelectManyFeatures: boolean; // true - rendering checkboxes, false - radio buttons
+  features: {
+    [featureKey: string]: {
+      answer: string;
+      name: string;
+      description: string;
+    };
+  };
 }
